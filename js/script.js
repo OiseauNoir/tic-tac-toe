c1b226ee59dd5255436df13258625697998e3aa3
jesse houghton <OiseauNoir>
2016-08-07 16:37:21 -0500

    changed branch name

--- js/script.js
+++ js/script.js
@@ -3,96 +3,104 @@ $(document).ready(function(){
 
 
 
-// //gameplay
-//
-// //1 initiliaze player 1
-//
-// var current_player;define player 1(x) and player 2(o)
+//gameplay
+
+//1 initiliaze player 1
+  // var current_player;define player 1(x) and player 2(o)
 var player = 1;
-//
-// //2 Create click event (two parts)
-$('.square').on('click', function(event){
 
-//alert('add symbol here');
+//2 Create click event
+$('.square').on('click', function(event) {
+  //alert('click');
 
-var selectedSquare = $('this');
-if(selectedSquare.hasClass('x') || (selectedSquare.hasClass('o')){
+//2a perform fnction adding symbol to board based on user ID (define above
+//2b provide error notification if cell already populated
+//2c toggle player ID between 1 & 2 (define above as var)
+
+var selectedSquare = $(this); //this = .square
+if(selectedSquare.hasClass('fa fa-times') || selectedSquare.hasClass('fa fa-circle-o')) {
   alert('Hey dummy! This square is already taken!');
-}else{
-  if(player===1){
-  selectedSquare.addClass('x');
-}else{
-  selectedSquare.addClass('o');
-}
-}
+} else {  //if free square
+  if(player===1) {
+    selectedSquare.addClass('fa fa-times');
+      if(winningConditions('fa fa-times')) {
+        alert ('Player ' + player + 'wins!');
+      } else {
+      player = 2;
+    }
+  } else {
+    selectedSquare.addClass('fa fa-circle-o');
+      if(winningConditions('fa fa-circle-o')) {
+        alert ('Player ' + player + 'wins!');
+      } else {
+      player = 1;
+      }
+    }
+  }
 });
-// $('.board').on('click', function()) {}...
-//
-// //2a perform fnction adding symbol to board based on user ID (define above as vars)
-//   function...
-//
-// //2b toggle player ID between 1 & 2 (define above as var)
-//
-// function changeTurn() alternate h2 span between 1 & 2...
-//
-// //4 enable repeat 1 through 3 ...(max 9 possibe turns)
-//
-//
-//
-//
-//
-// //gamelogic
-// //1 provide error notification if cell already populated
-//
-// alert('This square is already taken!')
-//
-// //2 define end game conditions (2):
-//
+//4 enable repeat 1 through 3 ...(max 9 possibe turns)
+
+
+
+
+//gamelogic
+
+//2 define end game conditions (2):
+function winningConditions(symbol) {
+  //rows
+  if ($('.topleft').hasClass(symbol) && $('.topmid').hasClass(symbol) && $('.topright').hasClass(symbol)) {
+    return true;
+  } else if ($('.midleft').hasClass(symbol) && $('.midmid').hasClass(symbol) && $('.midright').hasClass(symbol)) {
+    return true;
+  } else if ($('.btmleft').hasClass(symbol) && $('.btmmid').hasClass(symbol) && $('.btmright').hasClass(symbol)) {
+    return true;
+  //columns
+  } else if ($('.topleft').hasClass(symbol) && $('.midleft').hasClass(symbol) && $('.btmleft').hasClass(symbol)) {
+    return true;
+  } else if ($('.topmid').hasClass(symbol) && $('.midmid').hasClass(symbol) && $('.btmmid').hasClass(symbol)) {
+    return true;
+  } else if ($('.topright').hasClass(symbol) && $('.midright').hasClass(symbol) && $('.btmright').hasClass(symbol)) {
+    return true;
+  //diagonal
+  } else if ($('.topleft').hasClass(symbol) && $('.midmid').hasClass(symbol) && $('.btmright').hasClass(symbol)) {
+    return true;
+  } else if ($('.topright').hasClass(symbol) && $('.midmid').hasClass(symbol) && $('.btmleft').hasClass(symbol)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// var winConditions = [
+// [.topleft, .topmid, .topright],
+// [.midleft, .midmid, .midright],
+// [.btmleft, .btmmid, .btmright],
+// [.topleft, .midleft, .btmleft],
+// [.topmid, .midmid, .btmmid],
+// [.topright, .midright, .btmright],
+// [.topleft, .midmid, .btmright],
+// [.topright, .midmid, .btmleft],
+// ]
 // var endgame: winConditions || drawConditions
 //
-// //2a winning scenario (3 in a row)
-//
-// var winConditions = [
-// [.topleft, .topcenter, .topright],
-// [.midleft, .midcenter, .midright],
-// [.btmleft, .btmcenter, .btmright],
-// [.topleft, .midleft, .btmleft],
-// [.topcenter, .midcenter, .btmcenter],
-// [.topright, .midright, .btmright],
-// [.topleft, .midcenter, .btmright],
-// [.topright, .midcenter, .btmleft],
-// ]
-//
-// //2b tie (all space full && 2a=false)
+// 2a winning scenario (3 in a row)
+
+
+
+//2b tie (all space full && 2a=false)
 // var drawConditions... (winConditions not true & board full)
-//
-// //3 check for end game conditions
-//
+
+//3 check for end game conditions
+
 // function endgame(){}
 // loop through winConditions and check T/F
-//
-// //3 reset board after end of game
-// //4 increment winner score by 1
-//
+
+//3 reset board after end of game
+//4 increment winner score by 1
+
 // if winConditions are true,
 // then clear board and increment winner score +1
-//
+
 // else if drawconditions are true,
 // then clear board (no changes to winner score)
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
 });
